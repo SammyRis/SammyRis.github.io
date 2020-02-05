2794589a28f9068e689f3889eea2988f402ad043
Risling0802698 <0802698@sps-student.sk.ca>
2020-02-05 13:30:04 -0600

    Update sketch.js

--- cs30-p5js-template-master - Copy/sketch.js
+++ cs30-p5js-template-master - Copy/sketch.js
@@ -4,12 +4,36 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-
+let ex = 120;
+let why = 120;
+let r = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function mousePressed(){
+  if(r === 0) r = 255;  
+  else r = 0;
+}
+
 function draw() {
   background(220);
+  fill(r,0,0);
+  ellipse(ex, why, 40, 40);
+  
+  if(keyIsDown(LEFT_ARROW)) {
+    ex -= 3;
+  }
+
+  if(keyIsDown(RIGHT_ARROW)){
+    ex += 3;
+  }
+  if(keyIsDown(UP_ARROW)){
+    why -= 3;
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    why += 3;
+  }
+
 }
